c75230363be73e2698df1eae94a4b7b2b6a699f4
Angela Cartagena <acartagena@woolworths.com.au>
2020-06-25 10:55:00 +1000

    Added HelloGraphQL

---
+++ HelloGraphQL/HelloGraphQL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloGraphQL
+//
+//  Created by Cartagena (Contractor) Angela on 25/6/20.
+//  Copyright © 2020 Cartagena (Contractor) Angela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
